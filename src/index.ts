dec80b7989129016a50089b250f5521e4eb8e12d
Adam-Elmi <Adamcade123@gmail.com>
2026-03-30 04:33:54 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Public API - re-export reusable components
+export { SomMarkPlayground } from './playground/SomMarkPlayground';
 export { CustomEditor } from './playground/Editor';
 export { OutputPanel } from './playground/OutputPanel';
 export { CustomSelect } from './playground/CustomSelect';
