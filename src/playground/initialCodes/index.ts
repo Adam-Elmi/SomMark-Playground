d8fd104c40031456e86084bf6612c3e082e612d2
Adam-Elmi <Adamcade123@gmail.com>
2026-03-30 04:34:18 +0300

    Update index.ts

--- src/playground/initialCodes/index.ts
+++ src/playground/initialCodes/index.ts
@@ -2,10 +2,12 @@ import { htmlInitialCode } from './html';
 import { markdownInitialCode } from './markdown';
 import { mdxInitialCode } from './mdx';
 import { textInitialCode } from './text';
+import { jsonInitialCode } from './json';
 
 export const initialCodes: Record<string, string> = {
     html: htmlInitialCode,
     markdown: markdownInitialCode,
     mdx: mdxInitialCode,
-    text: textInitialCode
+    text: textInitialCode,
+    json: jsonInitialCode
 };
